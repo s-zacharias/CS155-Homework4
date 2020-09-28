a952f54675f503f8a7675a98487db495854afb1d
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-09-28 18:11:27 -0500

    initial commit

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int weight, height, BMI;
+
+            // collect weight and height from user
+            weight = Int32.Parse(Weight.Text);
+            height = Int32.Parse(Height.Text);
+
+            // calculate BMI
+            BMI = (weight * 720) / (height * height);
+
+            // display message with BMI
+
+            if (BMI < 15)
+            {
+                bmiDisplay.Background = Brushes.Blue;
+                MessageBox.Show("Your BMI is " + BMI);
+            } else if (BMI >= 15 && BMI <= 25)
+            {
+                bmiDisplay.Background = Brushes.Green;
+                MessageBox.Show("Your BMI is " + BMI);
+            } else
+            {
+                bmiDisplay.Background = Brushes.Yellow;
+                MessageBox.Show("Your BMI is " + BMI);
+            }
+            
+
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int headCount, tailCount;
+            String toss = Console.ReadLine();
+            double percentHead, percentTail;
+
+            Console.WriteLine("For each coin toss enter either 'h' for heads or 't' for tails.");
+            
+            for (let i = 1; i < 9; i++)
+            {
+                Console.WriteLine(`Toss ${i}`, toss);
+                if (toss == 'h')
+            {
+                headCount++;
+            } else if (toss == 't')
+            {
+                tailCount++;
+            }
+            }
+
+            percentHead = headCount / 8;
+            percentTail = tailCount / 8;
+            
+            Console.WriteLine("Number of heads: ", headCount);
+            Console.WriteLine("Number of tails: ", tailCount);
+            Console.WriteLine("Percent heads: ", percentHead);
+            Console.WriteLine("Percent tails: ", percentTail);
+
+        }
+    }
+}
